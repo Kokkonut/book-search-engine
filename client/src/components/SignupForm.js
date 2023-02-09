009b1549a175ba05c2d0c77fd5ff8a68ed81a421
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-02-09 17:09:00 +1030

    Converted signup to graphQL

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
@@ -11,6 +13,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,13 +31,17 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // const response = await createUser(userFormData);
 
-      const { token, user } = await response.json();
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+      const { token, user } = data.addUser;
+      // const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
     } catch (err) {
